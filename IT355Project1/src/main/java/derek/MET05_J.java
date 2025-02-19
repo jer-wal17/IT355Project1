4670761886f3c56733206966099d62a8d2e182db
Derek Reynolds <superDreynolds@gmail.com>
2025-02-19 10:47:05 -0600

    Derek - Implement 2 more of my rules
     - MET05_J and MET06_J implemented

--- IT355Project1/src/main/java/derek/MET05_J.java
+++ IT355Project1/src/main/java/derek/MET05_J.java
@@ -1,15 +1,45 @@
 // Written by Derek Reynolds
 // For IT355 - Secure Software Development
 
-// TODO: Rule 06: MET05-J. Ensure that constructors do not call overridable methods
+// Rule 06: MET05-J. Ensure that constructors do not call overridable methods
 // TODO: Recommendation 06: MET50-J. Avoid ambiguous or confusing uses of overloading
 
 package main.java.derek;
 
 public class MET05_J
 {
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        Sub sub = new Sub();
+    }
+
+    private static class Super 
+    {
+        public Super() 
+        {
+            isSkaDead();
+        }
+
+        // Noncompliant implementation:
+        // public void isSkaDead() 
+        // {
+        //     System.out.println("No!");
+        // }
+
+        // Compliant implementation: 
+        public final void isSkaDead() 
+        {
+            System.out.println("No!");
+        }
+    }
+
+    private static class Sub extends Super 
+    {
+        private String printStatement = "Yes!";
+        // Test code: will cause an error if compliant with rule
+        // public void isSkaDead() 
+        // {
+        //     System.out.println(printStatement);
+        // }
     }
 }
--- IT355Project1/src/main/java/derek/MET06_J.java
+++ IT355Project1/src/main/java/derek/MET06_J.java
@@ -1,15 +1,144 @@
 // Written by Derek Reynolds
 // For IT355 - Secure Software Development
 
-// TODO: Rule 06: MET06-J. Do not invoke overridable methods in clone()
+// Rule 06: MET06-J. Do not invoke overridable methods in clone()
 // TODO: Recommendation 06: MET52-J. Do not use the clone() method to copy untrusted method parameters
 
 package main.java.derek;
 
 public class MET06_J
 {
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        Sub sub = new MET06_J().new Sub();
+        try 
+        {
+            Sub copy = (Sub)sub.clone();
+            System.out.println(copy.toString());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private class Super implements Cloneable 
+    {
+        protected Band[] dataArray;
+
+        private Super() 
+        {
+            populateData();
+        }
+
+        public Object clone() throws CloneNotSupportedException 
+        {
+            final Super clone = (Super) super.clone();
+            clone.populateData();
+            clone.dataArray = clone.deepCopy();
+            return clone;
+        }
+
+        public String toString() 
+        {
+            String result = "";
+            for (int i = 0; i < dataArray.length; i++) 
+            {
+                result += dataArray[i].name + "\n";
+            }
+            return result;
+        }
+        // Noncompliant implementation:
+        // private Band[] deepCopy() throws CloneNotSupportedException 
+        // {
+        //     if (dataArray == null)
+        //     {
+        //         throw new NullPointerException();
+        //     }
+        //     Band[] dataArrayCopy = new Band[dataArray.length];
+        //     for (int i = 0; i < dataArrayCopy.length; i++) 
+        //     {
+        //         dataArrayCopy[i] = (Band) dataArray[i].clone();
+        //     }
+        //     return dataArrayCopy;
+        // }
+
+        // protected void populateData() 
+        // {
+        //     dataArray = new Band[5];
+        //     dataArray[0] = new Band("Reel Big Fish");
+        //     dataArray[1] = new Band("Streetlight Manifesto");
+        //     dataArray[2] = new Band("Catch 22");
+        //     dataArray[3] = new Band("Less Than Jake");
+        //     dataArray[4] = new Band("Goldfinger");
+        // }
+
+        // Compliant implementation: 
+        private final Band[] deepCopy() throws CloneNotSupportedException 
+        {
+            if (dataArray == null)
+            {
+                throw new NullPointerException();
+            }
+            Band[] dataArrayCopy = new Band[dataArray.length];
+            for (int i = 0; i < dataArrayCopy.length; i++) 
+            {
+                dataArrayCopy[i] = (Band) dataArray[i].clone();
+            }
+            return dataArrayCopy;
+        }
+
+        protected final void populateData() 
+        {
+            dataArray = new Band[5];
+            dataArray[0] = new Band("Reel Big Fish");
+            dataArray[1] = new Band("Streetlight Manifesto");
+            dataArray[2] = new Band("Catch 22");
+            dataArray[3] = new Band("Less Than Jake");
+            dataArray[4] = new Band("Goldfinger");
+        }
+    }
+
+    private class Sub extends Super 
+    {
+        private Sub() 
+        {
+            super();
+        }
+        
+        public Object clone() throws CloneNotSupportedException {
+            final Sub clone = (Sub) super.clone();
+            clone.populateData();
+            return clone;
+        }
+
+        // Test code: will cause an error if compliant with rule
+        // protected void populateData() 
+        // {
+        //     dataArray = new Band[1];
+        //     dataArray[0] = new Band("Kidz Bop");
+        // }
+    }
+
+    private class Band implements Cloneable 
+    {
+        private String name;
+
+        public Band(String bandName)
+        {
+            name = bandName;
+        }
+
+        public void setName(String bandName)
+        {
+            name = bandName;
+        }
+
+        public Object clone() throws CloneNotSupportedException 
+        {
+            final Band clone = new Band("");
+            clone.setName(this.name);
+            return clone;
+        }
     }
 }
