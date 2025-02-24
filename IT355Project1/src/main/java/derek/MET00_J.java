17f95314d7b256c773072efffc7c12322683c244
Derek Reynolds <superDreynolds@gmail.com>
2025-02-24 11:59:44 -0600

    Derek - Last rule implemented
    - Still need to implement 4 recommendations whoopsie!

--- IT355Project1/src/main/java/derek/MET00_J.java
+++ IT355Project1/src/main/java/derek/MET00_J.java
@@ -1,7 +1,7 @@
 // Written by Derek Reynolds
 // For IT355 - Secure Software Development
 
-// TODO: Rule 06: MET00-J. Validate method arguments
+// Rule 06: MET00-J. Validate method arguments
 
 package main.java.derek;
 
@@ -9,6 +9,23 @@ public class MET00_J
 {
     public static void main(String[] args) throws Exception
     {
-        System.out.println("Hello, World!");
+        System.out.println(multiple(Integer.MAX_VALUE, 2));
+    }
+    public static int multiple(int first, int second)
+    {
+        long result = (long) first * second;
+        // Noncompliant implementation:
+        // return (int) result;
+
+        // Compliant implementation:
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
+        {
+            System.out.println("MET00_J.multiple(): result is outside of the range of possible Integer values!");
+            return Math.clamp(result, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+        else
+        {
+            return (int) result;
+        }
     }
 }
