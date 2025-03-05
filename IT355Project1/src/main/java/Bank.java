a262fdd820f87c6fdc76854d72af18bd4e90524d
JD Waldron <jeromew1714@gmail.com>
2025-03-04 18:05:00 -0600

    Made menu for App.java and changes to BankAccount
    Switched floats to doubles and implemented deposit() and withdraw() in BankAccount

--- IT355Project1/src/main/java/Bank.java
+++ IT355Project1/src/main/java/Bank.java
@@ -4,22 +4,29 @@ public class Bank {
     private long uniqueId;
     private String name;
     private BankAccount[] accountList;
-    private long openedAccounts = 0;/*using this to generate a unique ID for accounts*/
+    private long openedAccounts = 0;/* using this to generate a unique ID for accounts */
+
     /**
      * Constructor for a bank object
+     * 
      * @param uniqueId unique identifier for the bank
-     * @param name name of the bank
+     * @param name     name of the bank
      */
-    public Bank(long uniqueId, String name){
+    public Bank(long uniqueId, String name) {
         this.uniqueId = uniqueId;
         this.name = name;
-        this.accountList = new BankAccount[10];/*just have this at 10 for now, we can change how this works at any point lol */
+        this.accountList = new BankAccount[10];/*
+                                                * just have this at 10 for now, we can change how this works at any
+                                                * point lol
+                                                */
     }
+
     /**
      * Copy constructor for a bank object
+     * 
      * @param bank a passed in bank object
      */
-    public Bank(Bank bank){
+    public Bank(Bank bank) {
         this.uniqueId = bank.getUniqueId();
         this.name = bank.getName();
         this.accountList = bank.getAccountList();
@@ -27,47 +34,58 @@ public class Bank {
     }
 
     /**
-     * opens an account in the bank if ones available using a User's id and a given balance
-     * @param userId user id that will be set to the owner of the bank account
+     * opens an account in the bank if ones available using a User's id and a given
+     * balance
+     * 
+     * @param userId  user id that will be set to the owner of the bank account
      * @param balance starting balance of the account
      */
-    public void openAccount(int userId, int balance){
-        /*find a open space and open account there*/
-        for (int i = 0; i < accountList.length;i++){
-            if (accountList[i] == null){
-                accountList[i] =  new BankAccount(openedAccounts++, uniqueId, userId, balance);
+    public BankAccount openAccount(int userId, double balance) {
+        /* find a open space and open account there */
+        BankAccount openedAccount = null;
+        for (int i = 0; i < accountList.length; i++) {
+            if (accountList[i] == null) {
+                openedAccount = new BankAccount(openedAccounts++, uniqueId, userId, balance);
+                accountList[i] = openedAccount;
                 break;
             }
         }
+        return openedAccount;
     }
+
     /**
-     * Method that looks for an open account that matches the userId and accountId given, and closes it if so.
-     * @param userId id of the owner of the account
+     * Method that looks for an open account that matches the userId and accountId
+     * given, and closes it if so.
+     * 
+     * @param userId    id of the owner of the account
      * @param accountId unique id of the account
      */
-    public void closeAccount(int userId, int accountId){
-        for (int i = 0; i < accountList.length;i++){
-            if (accountList[i] != null){
-                /*check if owner and account id are the same */
-                if (accountList[i].getOwnerId() == userId && accountList[i].getUniqueId() == accountId){
+    public void closeAccount(int userId, int accountId) {
+        for (int i = 0; i < accountList.length; i++) {
+            if (accountList[i] != null) {
+                /* check if owner and account id are the same */
+                if (accountList[i].getOwnerId() == userId && accountList[i].getUniqueId() == accountId) {
                     accountList[i] = null;
                     break;
                 }
             }
         }
     }
+
     /**
      * Method that returns a copy of the account with the given userid and accountId
-     * @param userId user id of the owner of the account
+     * 
+     * @param userId    user id of the owner of the account
      * @param accountId unique id of the account
      * @return bank account with passed in userid and accountid or null
      */
-    public BankAccount getAccount(int userId, int accountId){
-        for (int i = 0; i < accountList.length;i++){
-            if (accountList[i] != null){
-                /*check if owner and account id are the same */
-                if (accountList[i].getOwnerId() == userId && accountList[i].getUniqueId() == accountId){
-                    BankAccount returnBankAccount = new BankAccount(accountList[i].getUniqueId(), accountList[i].getBankId(), accountList[i].getOwnerId(), accountList[i].getBalance());
+    public BankAccount getAccount(int userId, int accountId) {
+        for (int i = 0; i < accountList.length; i++) {
+            if (accountList[i] != null) {
+                /* check if owner and account id are the same */
+                if (accountList[i].getOwnerId() == userId && accountList[i].getUniqueId() == accountId) {
+                    BankAccount returnBankAccount = new BankAccount(accountList[i].getUniqueId(),
+                            accountList[i].getBankId(), accountList[i].getOwnerId(), accountList[i].getBalance());
                     return returnBankAccount;
                 }
             }
@@ -75,27 +93,38 @@ public class Bank {
         return null;
     }
 
-    /**getters and setters that are needed */
+    /** getters and setters that are needed */
     public long getUniqueId() {
         return uniqueId;
     }
+
     public String getName() {
         return name;
     }
+
     public long getOpenedAccounts() {
         return openedAccounts;
     }
+
     /**
      * Method to return a deep copy of the Bank Account array
+     * 
      * @return a deep copy of the accountList
      */
     public BankAccount[] getAccountList() {
         BankAccount[] copiedList = new BankAccount[accountList.length];
-        for (int i = 0; i < accountList.length;i++){
-            if (accountList[i] != null){
-                copiedList[i] = new BankAccount(accountList[i].getUniqueId(), accountList[i].getBankId(), accountList[i].getOwnerId(), accountList[i].getBalance());
+        for (int i = 0; i < accountList.length; i++) {
+            if (accountList[i] != null) {
+                copiedList[i] = new BankAccount(accountList[i].getUniqueId(), accountList[i].getBankId(),
+                        accountList[i].getOwnerId(), accountList[i].getBalance());
             }
         }
         return copiedList;
+    }
+
+    public void printAccounts() {
+        for (int i = 0; i < openedAccounts; i++) {
+            System.out.print(accountList[i]);
+        }
     }
 }
--- IT355Project1/src/main/java/BankAccount.java
+++ IT355Project1/src/main/java/BankAccount.java
@@ -1,39 +1,60 @@
 package main.java;
 
-class BankAccount{
+import java.text.DecimalFormat;
+
+class BankAccount {
     private long uniqueId;
     private long bankId;
     private long ownerId;
-    private float balance;
+    private double balance;
 
     /**
      * Constructor class
-     * @param bankID bank account id
+     * 
+     * @param bankID  bank account id
      * @param ownerID owner account id
      * @param balance starting balance for the account
      */
-    public BankAccount(long uniqueId, long bankId, long ownerId, float balance){
+    public BankAccount(long uniqueId, long bankId, long ownerId, double balance) {
         this.uniqueId = uniqueId;
         this.bankId = bankId;
         this.ownerId = ownerId;
         this.balance = balance;
     }
+
     /**
      * Copy constructor for a bank account object
+     * 
      * @param account passed in bank account object
      */
-    public BankAccount(BankAccount account){
+    public BankAccount(BankAccount account) {
         this.uniqueId = account.getUniqueId();
         this.bankId = account.getBankId();
         this.ownerId = account.getOwnerId();
         this.balance = account.getBalance();
     }
-    
-    /*Getters and setters */
+
+    @Override
+    public String toString() {
+        String outString = "Unique ID: " + uniqueId + "\n";
+        outString += "Bank ID: " + bankId + "\n";
+        outString += "Owner ID: " + ownerId + "\n";
+        DecimalFormat df = new DecimalFormat("0.00");
+        outString += "Balance: $" + df.format(balance) + "\n";
+
+        return outString;
+    }
+
+    public String balanceAsString() {
+        DecimalFormat df = new DecimalFormat("0.00");
+        return "$" + df.format(this.getBalance());
+    }
+
+    /* Getters and setters */
     public long getUniqueId() {
         return uniqueId;
     }
-    
+
     public void setUniqueId(long uniqueId) {
         this.uniqueId = uniqueId;
     }
@@ -54,11 +75,23 @@ class BankAccount{
         this.ownerId = ownerId;
     }
 
-    public float getBalance() {
+    public double getBalance() {
         return balance;
     }
 
-    public void setBalance(float balance) {
+    public void setBalance(double balance) {
         this.balance = balance;
+    }
+
+    public double deposit(double amount) {
+        this.balance += amount;
+
+        return this.balance;
+    }
+
+    public double withdraw(double amount) {
+        this.balance -= amount;
+
+        return this.balance;
     }
 }
